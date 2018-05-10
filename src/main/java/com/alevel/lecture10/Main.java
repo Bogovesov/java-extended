c0a708d2e4f2206f061f43916d0757ff5326e002
Sergey Bogovesov <bogovesov@gmail.com>
2018-05-10 18:46:50 +0300

    Custom Array List

--- src/main/java/com/alevel/lecture10/Main.java
+++ src/main/java/com/alevel/lecture10/Main.java
@@ -1,29 +1,38 @@
 package com.alevel.lecture10;
 
-import com.alevel.lecture10.collection.CustomLinkedList;
+import com.alevel.lecture10.collection.CustomArrayList;
 import com.alevel.lecture10.model.Cat;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            CustomLinkedList<Cat> catCustomLinkedList = new CustomLinkedList<Cat>();
-            catCustomLinkedList.addFirst(new Cat(1, "cat1"));
-            catCustomLinkedList.addFirst(new Cat(2, "cat2"));
-            catCustomLinkedList.addFirst(new Cat(3, "cat3"));
-            catCustomLinkedList.addFirst(new Cat(4, "cat4"));
-            catCustomLinkedList.addFirst(new Cat(5, "cat5"));
-            catCustomLinkedList.addFirst(new Cat(6, "cat6"));
-            catCustomLinkedList.addFirst(new Cat(7, "cat7"));
+        CustomArrayList<Cat> catCustomArrayList = new CustomArrayList<Cat>();
 
-            boolean exit = false;
-            while (!exit) {
-                Cat cat = catCustomLinkedList.removeLast();
-                if (cat == null) {
-                    exit = true;
-                    break;
-                }
-                System.out.println(cat);
-            }
+        catCustomArrayList.add(new Cat(1, "Barsik1"));
+        catCustomArrayList.add(new Cat(2, "Barsik2"));
+        catCustomArrayList.add(new Cat(3, "Barsik3"));
+        catCustomArrayList.add(new Cat(4, "Barsik4"));
+        catCustomArrayList.add(new Cat(5, "Barsik5"));
+        catCustomArrayList.add(new Cat(6, "Barsik6"));
+        catCustomArrayList.add(new Cat(7, "Barsik7"));
+
+        Cat cat8 = new Cat(8, "Barsik8");
+        catCustomArrayList.add(3, cat8);
+
+        for (int i = 0; i < catCustomArrayList.length(); i++) {
+            Cat cat = catCustomArrayList.get(i);
+            System.out.println(cat);
         }
+        System.out.println(catCustomArrayList.contains(cat8));
+
+        System.out.println(catCustomArrayList.remove(3));
+        System.out.println(catCustomArrayList.contains(cat8));
+        System.out.println(catCustomArrayList.remove(5));
+
+        System.out.println("After delete");
+        for (int i = 0; i < catCustomArrayList.length(); i++) {
+            Cat cat = catCustomArrayList.get(i);
+            System.out.println(cat);
+        }
+        catCustomArrayList.clear();
     }
 }
